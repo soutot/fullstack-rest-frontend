5f010c6c7f6f74716486139a3e39b8aad349610a
Tiago Souto <soutot@users.noreply.github.com>
2018-08-15 16:24:31 -0300

    feat(core): add header and icon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
 
 import './App.css';
 import Routes from './router/routes';
+import favicon from './images/albelli.jpeg';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <Helmet>
+          <meta charSet='utf-8' />
+          <title>Albelli</title>
+          <link href='https://fonts.googleapis.com/icon?family=Material+Icons' rel='stylesheet' />
+          <meta name='description' content='albelli orders' />
+          <link rel='icon' type='image/png' href={favicon} sizes='16x16' />
+        </Helmet>
         <Routes />
       </React.Fragment>
     );
